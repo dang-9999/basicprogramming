f441fe9c2d6dc1a48af10bb21472ab09185222fb
Dang <dlekdms1026@naver.com>
2023-10-25 14:23:37 +0900

    다은수정(admin06)

--- cafe_kiosk/src/package_1/Admin.java
+++ cafe_kiosk/src/package_1/Admin.java
@@ -2,13 +2,11 @@ package package_1;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Admin {
 
